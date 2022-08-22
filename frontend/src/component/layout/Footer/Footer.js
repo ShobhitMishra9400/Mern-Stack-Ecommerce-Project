d58a6a56cd4ddf138b2a378bc664c126cab8a13e
ShobhitMishra9400 <82651240+ShobhitMishra9400@users.noreply.github.com>
2022-08-22 21:26:48 +0530

    Update Footer.js

--- frontend/src/component/layout/Footer/Footer.js
+++ frontend/src/component/layout/Footer/Footer.js
@@ -17,7 +17,7 @@ const Footer = () => {
         <h1>ECOMMERCE</h1>
         <p>High Quality is our first priority</p>
 
-        <p>Copyrights 2022 &copy; Shobhit</p>
+    
       </div>
 
       <div className="rightFooter">
